d9727189b3d51f58b2cc5c51c312bc5b3cb74d2c
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-05-29 22:07:31 +0530

    Time: 256 ms (22.89%), Space: 6 MB (82.77%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    void swap(int &a1, int &a2) {
+        int temp = a1;
+        a1 = a2;
+        a2 = temp;
+    }
+    
+    
+    int helper(int index, vector<int> &nums) {
+        if(index >= nums.size()) {
+            return 1;
+        }
+        int sum = 0;
+        for(int i = index; i < nums.size(); i++) {
+            swap(nums.at(index), nums.at(i));
+            if(nums.at(index) % (index + 1) == 0 || (index + 1) % nums.at(index) == 0) {
+                sum += helper(index + 1, nums);
+            } 
+            swap(nums.at(index), nums.at(i));
+        }
+        return sum;
+    } 
+    
+    
+    int countArrangement(int n) {
+        vector<int> nums;
+        for(int i = 1; i <= n; i++) {
+            nums.push_back(i);
+        }
+        return helper(0, nums);
+    }
+};
