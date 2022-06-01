7b44c805bd680fcc712badf0c78f907dab4aee0d
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-06-01 23:22:30 +0530

    Time: 27 ms (21.21%), Space: 8.1 MB (35.93%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<char, int> map;
+    int romanToInt(string s) {
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+        int res = 0;
+        int prev = INT_MIN;
+        for(int i = s.length() - 1; i>= 0;i--) {
+            int val = map[s.at(i)];
+            if(val < prev) {
+                res -= map[s.at(i)];
+            } else {
+                res += map[s.at(i)];
+            }
+            prev = val;
+        }
+        return res;
+        
+    }
+};
