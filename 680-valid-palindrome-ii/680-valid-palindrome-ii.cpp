26a16c4a006db8067c4e7d25a0e3f24fc0eb99ce
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-06-01 23:09:47 +0530

    Time: 86 ms (23.79%), Space: 24.1 MB (29.00%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -13,8 +13,6 @@ public:
     }
     
     bool validPalindrome(string s) {
-        int count = 0;
-        int start = 0, end = s.length() - 1;
         for(int i = 0, j = s.length() - 1; i<= j;) {
             if(s.at(i) != s.at(j)) {
                 if(isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1)) {
