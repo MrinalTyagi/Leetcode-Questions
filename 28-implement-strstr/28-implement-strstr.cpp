9ff318542e51a7f717183bdb75f8f862f7c4776f
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-06-02 09:33:13 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (22.48%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
+    
+    void lps(string p, int *arr) {
+        int i = 1, len = 0, n = p.length();
+        arr[0] = 0;
+        while(i < n) {
+            if(p.at(i) == p.at(len)) {
+                arr[i] = len + 1;
+                i++;
+                len++;
+            } else {
+                if(len == 0) {
+                    arr[i] = 0;
+                    i++;
+                } else {
+                    len = arr[len - 1];
+                }
+            }
+        }
+    }
+    
+    
     int strStr(string haystack, string needle) {
         if(needle.length() == 0) {
             return 0;
         }
         int n = haystack.length(), m = needle.length();
-        for(int i = 0; i < n - m + 1; i++) {
-            bool flag = true;
-            for(int j = 0; j < m; j++) {
-                if(haystack.at(i + j) != needle.at(j)) {
-                    flag = false;
-                    break;
-                }
+        int arr[needle.length()];
+        lps(needle, arr);
+        int i = 0, j = 0;
+        while(i < n) {
+            if(haystack.at(i) == needle.at(j)) {
+                i++;
+                j++;
             }
-            if(flag) {
-                return i;
+            if(j == m) {
+                return (i - j);
+                j = arr[j - 1];
+            } else if(i < n && haystack.at(i) != needle.at(j)) {
+                if(j == 0) {
+                    i++;
+                } else {
+                    j = arr[j - 1];
+                }
             }
         }
         return -1;
