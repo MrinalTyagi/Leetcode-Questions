8fc2556b5bde252244f5e8f288cbed0aed32eb09
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-06-10 14:02:57 +0530

    Time: 39 ms (35.61%), Space: 10.5 MB (81.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        string t = "";
         stack<int> st;
         for(int i = 0; i < s.length(); i++) {
             if(s.at(i) == '(') {
-                t += '(';
                 st.push(i);
             } else if(s.at(i) == ')') {
                 if(!st.empty() && s.at(st.top()) == '(') {
-                    t += ')';
                     st.pop();
+                } else {
+                    s.erase(i, 1);
+                    i--;
                 }
-            } else {
-                t+=s.at(i);
             }
         }
         while(!st.empty()) {
-            for(int i = t.length() - 1; i>= 0; i--) {
-                if(t.at(i) == s.at(st.top())) {
-                   t.erase(i, 1);
+            for(int i = s.length() - 1; i>= 0; i--) {
+                if(s.at(i) == s.at(st.top())) {
+                   s.erase(i, 1);
                     break;
                 }
             }
             st.pop();
         }
-        return t;
+        return s;
     }
 };
