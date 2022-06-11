f592dad8bce0f3151015a3b70c51760b994212a9
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-06-11 09:56:54 +0530

    Time: 11 ms (58.29%), Space: 8.9 MB (23.48%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     string intToRoman(int num) {
         string t = "";
-        vector<int> val = {1000, 500, 100, 50, 10, 5, 1};
-        vector<char> ch = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
-        for(int i = 0; i < val.size(); i+=2) {
+        vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> ch = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for(int i = 0; i < val.size(); i++) {
             int v = num / val.at(i);
             if(v > 0) {
                 int c = v * val.at(i);
-                if(v == 4) {
-                    v -= 4;
+                while(v--) {
                     t += ch.at(i);
-                    t += ch.at(i - 1);
-                } else if(v == 9) {
-                    v -= 9;
-                    t += ch.at(i);
-                    t += ch.at(i - 2);
-                } else if(v >= 5) {
-                    v -= 5;
-                    t += ch.at(i - 1);
-                }
-                string a(v, ch.at(i));
-                t += a;
-                num = num % (c);
+                } 
+                num = num % c;
             }
         }
         return t;
