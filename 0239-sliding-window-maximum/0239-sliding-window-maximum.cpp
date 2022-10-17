395320444bc73586950c85586813b5154c81d5c3
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2022-10-17 12:23:34 +0530

    Time: 1274 ms (5.67%), Space: 212.1 MB (6.56%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        multiset<int, greater<int>> pq;
+        for(int i = 0; i < k; i++) {
+            pq.insert(nums[i]);
+        }
+        for(int i = k; i < nums.size(); i++) {
+            res.push_back(*pq.begin());
+            pq.erase(pq.lower_bound(nums[i - k]));
+            pq.insert(nums[i]);
+        }
+        res.push_back(*pq.begin());
+        return res;
+    }
+};
