3ea16a45cb211e89cbef50b0754cd99b3b940c04
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2023-01-01 18:50:11 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -6,26 +6,20 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int helper(vector<int> &height, int n, vector<int> &dp) {
-        if(n == 0){
-            return 0;
-        }
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        int first = helper(height, n - 1, dp) + abs(height[n] - height[n - 1]);
-        int second = INT_MAX;
-        if(n - 2 >= 0) {
-            second = helper(height, n - 2, dp) + abs(height[n] - height[n - 2]);
-        }
-        int res = min(first, second);
-        return dp[n] = res;
-    }
-  
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
         vector<int> dp(n, -1);
-        return helper(height, n - 1, dp);
+        dp[0] = 0;
+        for(int i = 1; i < n; i++) {
+            int first = dp[i - 1] + abs(height[i] - height[i - 1]);
+            int second = INT_MAX;
+            if(i - 2 >= 0) {
+                second = dp[i - 2] + abs(height[i] - height[i - 2]);
+            }
+            int res = min(first, second);
+            dp[i] = res;
+        }
+        return dp[n - 1];
     }
 };
 
