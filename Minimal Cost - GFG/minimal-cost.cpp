e72cf27ea678a03b8c1e6afbe7bce8f3d48420e6
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2023-01-02 08:34:00 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,27 +6,21 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int helper(vector<int>& height, int n, int k, vector<int> &dp) {
-        if(n == 0) {
-            return 0;
-        }
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        int res = INT_MAX;
-        for(int i = 1; i <= k; i++) {
-            if(n - i >= 0) {
-                int inter = helper(height, n - i, k, dp) + abs(height[n] - height[n - i]);
-                res = min(res, inter);
-            }
-        }
-        return dp[n] = res;
-    }
-
     int minimizeCost(vector<int>& height, int n, int k) {
         // Code here
-        vector<int> dp(n, -1);
-        return helper(height, n - 1, k, dp);
+        vector<int> dp(n, 0);
+        dp[0] = 0;
+        for(int i = 1; i < n; i++) {
+            int res = INT_MAX;
+            for(int j = 1; j <= k; j++) {
+                if(i - j >= 0) {
+                    int inter = dp[i - j] + abs(height[i] - height[i - j]);
+                    res = min(res, inter);
+                }
+            }
+            dp[i] = res;
+        }
+        return dp[n - 1];
     }
 };
 
